b9576fae22e8ea017053847d095cf0dad06f6ae3
Nazar Khlivnyi <135509585+NazarKhl@users.noreply.github.com>
2024-12-24 14:35:03 +0100

    Create main .html files for app

---
+++ package-data/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./**/*.{html,js,jsx,ts,tsx,vue,svelte}", // Рекурсивно знаходить усі HTML, JS, TS, JSX тощо
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
